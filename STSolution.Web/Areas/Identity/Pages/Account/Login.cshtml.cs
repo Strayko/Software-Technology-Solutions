208cecff3d412069406cc011a60d8f89789fdaa6
Strayko <strayko_lol@windowslive.com>
2020-09-30 21:54:07 +0200

    setup admin panel

--- STSolution.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ STSolution.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -73,7 +73,7 @@ namespace STSolution.Web.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/Admin");
 
             if (ModelState.IsValid)
             {
---
+++ STSolution.Web/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ST.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
