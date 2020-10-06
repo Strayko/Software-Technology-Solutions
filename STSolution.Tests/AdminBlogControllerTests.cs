f6ed0062a690db4588f04b077b10a9062fab7eb3
Strayko <strayko_lol@windowslive.com>
2020-10-06 21:53:52 +0200

    get edit from controller test

---
+++ STSolution.Tests/AdminBlogControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using ST;
+using ST.Controllers;
+
+namespace STSolution.Tests
+{
+    [TestFixture]
+    public class AdminBlogControllerTests
+    {
+        [Test]
+        public void GetEdit_FromController_ReturnSingleBlog()
+        {
+            var mock = new Mock<IBlogRepository>();
+            mock.Setup(b => b.GetBlogById(It.IsAny<int>()));
+
+            var adminBlogController = new AdminBlogController(mock.Object);
+            adminBlogController.Edit(It.IsAny<int>());
+
+            mock.Verify(b => b.GetBlogById(It.IsAny<int>()), Times.Once);
+        }
+    }
+}
