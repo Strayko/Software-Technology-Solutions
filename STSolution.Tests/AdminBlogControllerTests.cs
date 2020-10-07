ce40da0487650763cfef1a58870d4935b06ded2a
Strayko <strayko_lol@windowslive.com>
2020-10-07 16:33:57 +0200

    three new tests for adminBlogController

--- STSolution.Tests/AdminBlogControllerTests.cs
+++ STSolution.Tests/AdminBlogControllerTests.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using ST;
@@ -9,6 +13,54 @@ namespace STSolution.Tests
     [TestFixture]
     public class AdminBlogControllerTests
     {
+        
+        Blog blog = new Blog
+        {
+            BlogId = 1,
+            Name = "",
+            ShortDescription = "value",
+            LongDescription = "value",
+            ImageUrl = "value",
+            ImageThumbnailUrl = "",
+            Category = null,
+            Notes = "",
+            CategoryId = 2
+        };
+
+        [Test]
+        public void GetIndex_FromController_ReturnAllBlogs()
+        {
+            var mock = new Mock<IBlogRepository>();
+            mock.Setup(b => b.AllBlogs);
+
+            var adminBlogController = new AdminBlogController(mock.Object);
+            adminBlogController.Index();
+
+            mock.Verify(b => b.AllBlogs, Times.Once);
+        }
+
+        [Test]
+        public void GetCreate_FromController_ReturnView()
+        {
+            var mock = new Mock<IBlogRepository>();
+
+            AdminBlogController adminBlogController = new AdminBlogController(mock.Object);
+            IActionResult result = adminBlogController.Create();
+
+            Assert.AreEqual(typeof(ViewResult), result.GetType());
+        }
+
+        [Test]
+        public void PostCreate_FromController_SaveBlogToDatabase()
+        {
+            var mock = new Mock<IBlogRepository>();
+            var adminBlogController = new AdminBlogController(mock.Object); 
+            
+            adminBlogController.Create(blog);
+
+            mock.Verify(b => b.Add(blog));
+        }
+
         [Test]
         public void GetEdit_FromController_ReturnSingleBlog()
         {
